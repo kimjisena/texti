435100add633e03280735c946ab013baa1bfb003
Kim Jisena <kimjisena@gmail.com>
2024-01-27 20:04:00 +0300

    misc: minor changes

--- texti.c
+++ texti.c
@@ -13,6 +13,8 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+#define TEXTI_VERSION "0.1.0"
+
 /** data **/
 
 struct editorConfig
@@ -185,7 +187,27 @@ void editorDrawRows(struct abuf *ab)
   int y;
   for (y = 0; y < E.screenrows; y++)
   {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3)
+    {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome), "Texti editor -- version %s", TEXTI_VERSION);
+
+      if (welcomelen > E.screencols)
+        welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding)
+      {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--)
+        abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    }
+    else
+    {
+      abAppend(ab, "~", 1);
+    }
 
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1)
