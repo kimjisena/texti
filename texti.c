2718cfa686571d18a896de5a10383409f32c6894
Kim Jisena <kimjisena@gmail.com>
2024-01-27 17:03:06 +0300

    misc: minor changes

--- texti.c
+++ texti.c
@@ -6,6 +6,10 @@
 #include <stdlib.h>
 #include <errno.h>
 
+/** defines **/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /** data **/
 struct termios orig_termios;
 
@@ -52,6 +56,34 @@ void enableRawMode()
   }
 }
 
+char editorReadKey()
+{
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+  {
+    if (nread == -1 && errno != EAGAIN)
+      die("read");
+  }
+  return c;
+}
+
+/** input **/
+
+void editorProcessKeypress()
+{
+  char c = editorReadKey();
+
+  switch (c)
+  {
+  case CTRL_KEY('q'):
+    exit(0);
+    break;
+
+  default:
+    break;
+  }
+}
 /** init **/
 
 int main()
@@ -60,22 +92,7 @@ int main()
 
   while (1)
   {
-    char c = '\0';
-
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-    {
-      die("read");
-    }
-    if (iscntrl(c))
-    {
-      printf("%d\r\n", c);
-    }
-    else
-    {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q')
-      break;
+    editorProcessKeypress();
   }
   return 0;
 }
