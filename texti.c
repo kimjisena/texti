58c6e42829828c54bee0899ee6c10e3f808bcccc
Kim Jisena <kimjisena@gmail.com>
2024-01-27 17:28:57 +0300

    misc: minor changes

--- texti.c
+++ texti.c
@@ -19,6 +19,9 @@ struct termios orig_termios;
 
 void die(const char *s)
 {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -79,6 +82,8 @@ void editorProcessKeypress()
   switch (c)
   {
   case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
 
