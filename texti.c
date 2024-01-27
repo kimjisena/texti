47e5fee23b3f249961bea2e96877fa382999ade1
Kim Jisena <kimjisena@gmail.com>
2024-01-27 15:53:38 +0300

    misc: minor changes

--- texti.c
+++ texti.c
@@ -19,8 +19,8 @@ void enableRawMode()
   struct termios raw = orig_termios;
 
   tcgetattr(STDIN_FILENO, &raw);
-
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+  raw.c_iflag &= ~(IXON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
